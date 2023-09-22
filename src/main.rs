bb4dced5085abdbf3a2b7c58d5007ff1bd5cdc6c
progermaks <maksimdavydenko12@gmail.com>
2023-09-22 19:15:57 +0700

    notebook test

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use std::time::Instant;
 use crate::quarter_of::quarter_of;
 
 mod quarter_of;
 mod warn_the_sheep;
 
 fn main() {
-    println!("{:?}",quarter_of(11))
+    let now = Instant::now();
+    println!("{:?}",quarter_of(11));
+    println!("{:?}", now.elapsed())
 }
