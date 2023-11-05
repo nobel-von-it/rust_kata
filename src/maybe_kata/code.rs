31f9aa41f32fa75f21d2c4ada671e68290cae92e
progermaks <maksimdavydenko12@gmail.com>
2023-11-05 23:25:05 +0700

    11.05.2023

--- src/maybe_kata/code.rs
+++ src/maybe_kata/code.rs
@@ -2,7 +2,8 @@ pub mod kyu8 {
 
 }
 pub mod kyu6 {
-    use std::collections::BTreeSet;
+    use std::collections::{BTreeSet, HashMap, HashSet};
+    use itertools::Itertools;
 
     pub fn find_odd_set(arr: &[i32]) -> i32 {
         let mut set = BTreeSet::new();
@@ -22,6 +23,33 @@ pub mod kyu6 {
     }
     pub fn count_bits(n: i64) -> u32 {
         format!("{:b}", n).matches('1').count() as u32
+    }
+    pub fn count_duplicates(text: &str) -> u32 {
+        let mut set = HashSet::new();
+        let mut res = 0;
+        for i in text.to_lowercase().split("") {
+            if !set.insert(i) {
+                res += 1
+            }
+        }
+        res
+    }
+    pub fn count_duplicates_hashmap(text: &str) -> u32 {
+        let mut map: HashMap<char, u16> = HashMap::new();
+        let mut res: u32 = 0;
+        for i in text.trim().to_lowercase().chars() {
+            let v = map.entry(i).or_insert(0);
+            *v += 1;
+        }
+        for (_, v) in map {
+            if v > 1 {
+                res += 1
+            }
+        }
+        res
+    }
+    pub fn count_duplicates_one_line(text: &str) -> u32 {
+        text.to_lowercase().chars().counts().values().filter(|&&i| i > 1).count() as u32
     }
 }
 pub mod kyu7 {
@@ -52,9 +80,10 @@ pub mod test {
     #[test]
     fn test() {
         let now = Instant::now();
-        // assert_eq!(code::kyu6::find_odd_xor(&[1,2,2,3,3,3,4,3,3,3,2,2,1]), 4);
+        assert_eq!(code::kyu6::find_odd_xor(&[1,2,2,3,3,3,4,3,3,3,2,2,1]), 4);
         // assert_eq!(code::kyu7::high_and_low("1 2 3"), "3 1");
-        assert_eq!(code::kyu6::count_bits(1234), 5);
+        // assert_eq!(code::kyu6::count_bits(1234), 5);
+        // assert_eq!(code::kyu6::count_duplicates_one_line("abcaaaa"), 1);
         println!("{:?}", now.elapsed())
     }
 }
