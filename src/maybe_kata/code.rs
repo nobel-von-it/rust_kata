263ff177d79697c8b93c9f7acef40fe09bb25466
progermaks <maksimdavydenko12@gmail.com>
2023-11-20 09:36:08 +0700

    (20.11.2023) ...

--- src/maybe_kata/code.rs
+++ src/maybe_kata/code.rs
@@ -69,6 +69,9 @@ pub mod kyu7 {
             _ => false,
         }
     }
+    pub fn is_leap_year(year: i32) -> bool {
+        (year % 4 == 0 && year % 100 != 0) || year % 400 == 0
+    }
 }
 
 pub mod test {
