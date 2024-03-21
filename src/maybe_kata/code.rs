8c86bc3761a4064bae3a9b9d2f6ed0bc990f87c2
nobel-von-it <maksimdavydenko12@gmail.com>
2024-03-21 11:10:57 +0700

    fixing smth

--- src/maybe_kata/code.rs
+++ src/maybe_kata/code.rs
@@ -29,7 +29,7 @@ pub mod kyu5 {
 }
 pub mod kyu4 {}
 pub mod kyu6 {
-    use std::collections::{BTreeMap, HashMap, HashSet};
+    use std::collections::{HashMap, HashSet};
 
     pub fn is_pangram(s: &str) -> bool {
         s.to_lowercase()
@@ -81,27 +81,5 @@ pub mod kyu7 {
         ch.sort();
         ch.reverse();
         ch.join("").parse::<u64>().unwrap()
-    }
-}
-
-pub mod test {
-    use crate::maybe_kata::code;
-    use std::time::Instant;
-
-    #[test]
-    fn test() {
-        let now = Instant::now();
-        // assert_eq!(code::kyu6::find_odd_xor(&[1,2,2,3,3,3,4,3,3,3,2,2,1]), 4);
-        // assert_eq!(code::kyu7::high_and_low("1 2 3"), "3 1");
-        // assert_eq!(code::kyu6::count_bits(1234), 5);
-        // assert_eq!(code::kyu6::count_duplicates_one_line("abcaaaa"), 1);
-        // assert_eq!(code::kyu5::beeramid(5000, 3.0), 16);
-        // assert_eq!(code::kyu5::beeramid(4, 4.0), 1);
-        // assert_eq!(code::kyu5::beeramid(9, 2.0), 1);
-        // assert_eq!(
-        //     code::kyu4::format_duration(3661),
-        //     "1 hour, 1 minute and 1 second"
-        // );
-        println!("{:?}", now.elapsed())
     }
 }
